aa23346dfb7bd9b838a093d178a8d82c82891aab
Costath <thalesdaniel@gmail.com>
2019-05-22 16:14:59 -0400

    Enum example
    Enum type example implementation

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    enum Day { Mon, Tue, Wed, Thu, Fri, Sat, Sun }
+    [Flags]
+    enum Genre { None = 0, Comedy = 1, Action = 2, Documentary = 4, Sci_Fi = 8, Romance = 16, Adult = 32}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GenreDemo();
+
+            //create the objects
+            Time a = new Time(9, 35);
+            Console.WriteLine(a);
+
+            Time b = new Time(18, 5);
+            Console.WriteLine(b);
+
+            Time c = new Time(28, 500);
+            Console.WriteLine(c);
+
+            Time d = new Time(10);
+            Console.WriteLine(d);
+
+            // create a list to store the objects
+            List<Time> times = new List<Time>() { a, b, c, d };
+
+            //display all the objects
+            Console.WriteLine("\n\nNormal time format");
+            foreach (Time t in times)
+            {
+                Console.WriteLine(t);
+            }
+
+            //change the format of the output
+            Console.WriteLine("\n\nMilitary time format");
+            //SetFormat(TimeFormat) is a class member, so you need the type to access it
+            Time.SetTimeFormat(TimeFormat.Mil);
+            //again display all the objects
+            foreach (Time t in times)
+            {
+                Console.WriteLine(t);
+            }
+
+            //change the format of the output
+            Console.WriteLine("\n\n24 hour time format");
+            Time.SetTimeFormat(TimeFormat.Hour24);
+            foreach (Time t in times)
+            {
+                Console.WriteLine(t);
+            }
+
+
+
+
+        }
+        static void GenreDemo()
+        {
+            Genre genre = Genre.None;
+            Console.WriteLine($"-> {genre}");
+            if (genre == Genre.Action)
+                Console.WriteLine("ACTION");
+            else
+                Console.WriteLine("NOT ACTION");
+
+            genre = Genre.Comedy | Genre.Romance | Genre.Action;      //combining multiple genres (bitwise OR operator)
+            Console.WriteLine($"-> {genre}");
+
+            if (genre.HasFlag(Genre.Action))
+                Console.WriteLine("ACTION");
+            else
+                Console.WriteLine("NOT ACTION");
+        }
+        static void DayDemo()
+        {
+            Day today = Day.Wed;
+            Console.WriteLine($"Today: {today}");
+
+            today += 1;
+            Console.WriteLine($"Today: {today}");
+
+            today += 1;
+            Console.WriteLine($"Today: {today}");
+
+            Console.WriteLine("Enter a day: ");
+            string input = Console.ReadLine();
+
+            today = (Day)Enum.Parse(typeof(Day), input);
+            Console.WriteLine($"Today: {today}");
+        }
+    }
+}
---
+++ EnumDemo/Time.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    enum TimeFormat { Mil, Hour12, Hour24}
+
+    class Time
+    {
+        static TimeFormat TIME_FORMAT = TimeFormat.Hour12;
+        
+        public int Hour { get; private set; }
+        public int Minute { get; private set; }
+
+        public Time (int hours = 0, int minutes = 0)
+        {
+            if (hours <= 24)
+                Hour = hours;
+            else
+                Hour = 0;
+
+            if (minutes <= 60)
+                Minute = minutes;
+            else
+                Minute = 0;
+        }
+
+        public override string ToString()
+        {
+            switch (TIME_FORMAT)
+            {
+                case TimeFormat.Mil:
+                    return $"{Hour:d2}{Minute:d2} hrs";
+                case TimeFormat.Hour24:
+                    return $"{Hour:d2}:{Minute:d2} hrs";
+                default:
+                    return $"{Hour % 12}:{Minute:d2} {(Hour < 12 ? "AM" : "PM")}";
+            }
+        }
+
+        public static void SetTimeFormat(TimeFormat time_format)
+        {
+            switch (time_format)
+            {
+                case TimeFormat.Mil:
+                    TIME_FORMAT = TimeFormat.Mil;
+                    break;
+                case TimeFormat.Hour12:
+                    TIME_FORMAT = TimeFormat.Hour12;
+                    break;
+                case TimeFormat.Hour24:
+                    TIME_FORMAT = TimeFormat.Hour24;
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
